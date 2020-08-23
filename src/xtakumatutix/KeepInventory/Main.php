6de8bf79d3b5daccc0d9d845ba7de306ad0f9d1b
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-08-23 15:40:19 +0900

    Add files via upload

---
+++ src/xtakumatutix/KeepInventory/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace xtakumatutix\KeepInventory;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Server;
+
+class Main extends PluginBase implements Listener
+{
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDeath(PlayerDeathEvent $event)
+    {
+        $event->setKeepInventory(true);
+    }
+}
